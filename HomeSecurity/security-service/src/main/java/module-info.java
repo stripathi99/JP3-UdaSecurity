cf7b345b1d7d01a65e3626cda449afe8688e9db8
Shubham Tripathi <shubham.tripathi4@outlook.com>
2022-11-23 01:39:39 +0530

    divided project into two separate modules: image and security

---
+++ HomeSecurity/security-service/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.udacity.security {
+  requires com.udacity.image;
+  requires miglayout;
+  requires java.desktop;
+  requires java.prefs;
+  requires com.google.common;
+  requires com.google.gson;
+  opens com.udacity.security.data to com.google.gson;
+}
